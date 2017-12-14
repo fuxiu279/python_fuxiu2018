cceab6a601b3b9b3c6c02de33b4ddd00c1cf99ef
fuxiu279 <fuxiu279@163.com>
2017-12-14 15:14:26 +0800

    lesson 3

--- lesson03/aishiqiuzhishu.py
+++ lesson03/aishiqiuzhishu.py
@@ -1,4 +1,4 @@
-#埃拉托色尼筛选法求质数
+#埃拉托色尼筛选法求质数，只适用于n小于100时
 
 def aishizhishu(n):
     ls =[2,3,5,7]
@@ -10,3 +10,29 @@ def aishizhishu(n):
 
 print(aishizhishu(100))
        
+
+import math
+
+# 埃拉托色尼筛法
+def zhishu(max):
+    li = []
+    for i in range(2, max + 1):
+        if i > 2 and i % 2 == 0:
+            li.append(0)
+        else:
+            li.append(i)
+
+    for i in range(3, int(math.sqrt(max)) + 1):
+        if li[i - 2] != 0:
+            for j in range(i + i, max + 1, i):
+                li[j - 2] = 0
+
+    outp=[]
+    for i in li:
+        if i != 0:
+            outp.append(i)
+
+    return outp
+
+
+print(zhishu(100))
